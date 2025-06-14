880d46a16a5017a6c4ce293e51825a77b995f68d
pranjal <pjlv1007@gmail.com>
2025-06-15 00:56:39 +0530

    Add Video model

---
+++ videotube-backend/models/Video.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const VideoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  uploaderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  comments: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
